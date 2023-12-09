4e7cfbcfc17ca8ebec2862638cd33fc1e8cdad09
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-12-10 01:46:01 +0700

    first commit

---
+++ src/main/java/com/example/eadjavawebspringmvcexamsem4ag/eadjavawebspringmvcexamsem4Application.java
@@ -0,0 +1,13 @@
+package com.example.eadjavawebspringmvcexamsem4ag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class eadjavawebspringmvcexamsem4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(eadjavawebspringmvcexamsem4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eadjavawebspringmvcexamsem4ag/IasfJavawebSpringmvcExamSem4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eadjavawebspringmvcexamsem4ag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasfJavawebSpringmvcExamSem4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
